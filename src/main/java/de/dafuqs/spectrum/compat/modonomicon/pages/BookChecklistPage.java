f4e1e3c8a3867819d1943b5da93fb137fc89761d
EgrorBs <tujh.2003@mail.ru>
2025-07-01 16:08:57 +0300

    fix(dirty): Client can't decode book sync packet
    - Revert 36030f6 ("what happened" pages now have consistent numbering)
    - Sorry, idk how to properly fix this

--- src/main/java/de/dafuqs/spectrum/compat/modonomicon/pages/BookChecklistPage.java
+++ src/main/java/de/dafuqs/spectrum/compat/modonomicon/pages/BookChecklistPage.java
@@ -18,13 +18,11 @@ import java.util.*;
 
 public class BookChecklistPage extends BookTextPage {
 	
-	private final int startCountingAt;
 	private final Map<ResourceLocation, BookTextHolder> checklist;
 	
-	public BookChecklistPage(BookTextHolder title, BookTextHolder text, boolean useMarkdownInTitle, boolean showTitleSeparator, String anchor, BookCondition condition, Map<ResourceLocation, BookTextHolder> checklist, int startCountingAt) {
+	public BookChecklistPage(BookTextHolder title, BookTextHolder text, boolean useMarkdownInTitle, boolean showTitleSeparator, String anchor, BookCondition condition, Map<ResourceLocation, BookTextHolder> checklist) {
         super(title, text, useMarkdownInTitle, showTitleSeparator, anchor, condition);
         this.checklist = checklist;
-		this.startCountingAt = startCountingAt;
     }
 	
 	public static BookChecklistPage fromJson(ResourceLocation entryId, JsonObject json, HolderLookup.Provider provider) {
@@ -33,8 +31,7 @@ public class BookChecklistPage extends BookTextPage {
 		var showTitleSeparator = GsonHelper.getAsBoolean(json, "show_title_separator", true);
         var text = BookGsonHelper.getAsBookTextHolder(json, "text", BookTextHolder.EMPTY, provider);
 		var anchor = GsonHelper.getAsString(json, "anchor", "");
-		int startCountingAt = GsonHelper.getAsInt(json, "start_counting_at", 1);
-		var condition = json.has("condition")
+        var condition = json.has("condition")
                 ? BookCondition.fromJson(entryId, json.getAsJsonObject("condition"), provider)
                 : new BookNoneCondition();
 		var checklistObject = GsonHelper.getAsJsonObject(json, "checklist", new JsonObject());
@@ -43,7 +40,7 @@ public class BookChecklistPage extends BookTextPage {
             var value = BookGsonHelper.getAsBookTextHolder(checklistObject, key, BookTextHolder.EMPTY, provider);
 			checklist.put(ResourceLocation.parse(key), value);
         }
-		return new BookChecklistPage(title, text, useMarkdownInTitle, showTitleSeparator, anchor, condition, checklist, startCountingAt);
+        return new BookChecklistPage(title, text, useMarkdownInTitle, showTitleSeparator, anchor, condition, checklist);
     }
 	
 	public static BookChecklistPage fromNetwork(RegistryFriendlyByteBuf buffer) {
@@ -52,7 +49,6 @@ public class BookChecklistPage extends BookTextPage {
         var showTitleSeparator = buffer.readBoolean();
         var text = BookTextHolder.fromNetwork(buffer);
 		var anchor = buffer.readUtf();
-		int startCountingAt = buffer.readInt();
         var condition = BookCondition.fromNetwork(buffer);
 
         // Because modonomicon decided RegistryByteBuf was worthwhile
@@ -63,8 +59,8 @@ public class BookChecklistPage extends BookTextPage {
             var value = BookTextHolder.fromNetwork(buffer);
             checklist.put(key, value);
         }
-		
-		return new BookChecklistPage(title, text, useMarkdownInTitle, showTitleSeparator, anchor, condition, checklist, startCountingAt);
+
+        return new BookChecklistPage(title, text, useMarkdownInTitle, showTitleSeparator, anchor, condition, checklist);
     }
 	
 	public Map<ResourceLocation, BookTextHolder> getChecklist() {
@@ -76,8 +72,8 @@ public class BookChecklistPage extends BookTextPage {
         super.prerenderMarkdown(textRenderer);
 		
 		List<MutableComponent> mutableTexts = new ArrayList<>();
-		
-		int i = startCountingAt;
+
+        int i = 1;
 		for (Map.Entry<ResourceLocation, BookTextHolder> entry : checklist.entrySet()) {
             BookTextHolder entryText = entry.getValue();
 			List<MutableComponent> rendered = textRenderer.render(entryText.getString());
